bce3b3613561c77e4154d6e868e2cd74b14ecd7e
MARCROCK22 <marcos22dev@gmail.com>
2022-06-11 17:31:00 -0400

    eval command

---
+++ src/bot/interactions/chat/commands/dev/eval.ts
@@ -0,0 +1,48 @@
+import { ApplicationCommandOptionType } from 'discord-api-types/v10';
+import { inspect } from 'util';
+import { API } from '../../../../../api.js';
+console.log(inspect(API, { depth: null }));
+import { BaseCommand, DCommand, DCommandOptions, type Interaction } from '../../base.js';
+
+@DCommandOptions({
+    isEphemeral: false,
+    needDefer: true
+})
+@DCommand({
+    name: 'eval',
+    description: 'evaluate',
+    options: [{
+        type: ApplicationCommandOptionType.String,
+        description: 'testxdxd',
+        name: 'code',
+        required: true,
+    }]
+})
+export default class Test extends BaseCommand {
+
+    onBefore(interaction: Interaction): boolean | Promise<boolean> {
+        return interaction.user.id === '507367752391196682';
+    }
+
+    onCancel(interaction: Interaction) {
+        return interaction.editOrCreateResponse({
+            content: 'no.'
+        });
+    }
+
+    async run(interaction: Interaction) {
+        const code = interaction.getString('code')!;
+
+        try {
+            let res = await eval(code);
+            await interaction.editOrCreateResponse({
+                content: '```js\n' + inspect(res) + '```'
+            });
+        } catch (e) {
+            await interaction.editOrCreateResponse({
+                content: '```js\n' + e + '```'
+            });
+        }
+
+    }
+}
