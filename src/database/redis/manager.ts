a22c16bd486146d6ec852cac6fe15800da40e377
MARCROCK22 <marcos22dev@gmail.com>
2022-06-11 14:30:46 -0400

    unnecessary async

--- src/database/redis/manager.ts
+++ src/database/redis/manager.ts
@@ -1,7 +1,4 @@
-import { AsyncQueue } from '@sapphire/async-queue';
 import { client as redisClient } from './client.js';
-
-export const RedisManagerQueue = new AsyncQueue();
 
 export const RedisManager = {
     client: redisClient,
@@ -12,16 +9,13 @@ export const RedisManager = {
         const res = time
             ? await this.client.setex(key, time, typeof value === 'object' ? JSON.stringify(value) : value)
             : await this.client.set(key, typeof value === 'object' ? JSON.stringify(value) : value);
-        RedisManagerQueue.shift();
         return res;
     },
-    async get(key: string) {
-        const res = await this.client.get(key);
-        return res;
+    get(key: string) {
+        return this.client.get(key);
     },
-    async delete(key: string) {
-        const res = await this.client.del(key);
-        return res;
+    delete(key: string) {
+        return this.client.del(key);
     },
     async deleteAllKeysMatched(match: string) {
         let pipeline = this.client.pipeline();
